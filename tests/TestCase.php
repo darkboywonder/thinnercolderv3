8a5fe16749ec7d4ca72dd65a9c9d1d94b5c90b83
Shawn Jones <shawn.greg.jones@gmail.com>
2019-11-06 10:55:33 -0600

    setup test suite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,43 @@
 
 namespace Tests;
 
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Foundation\Testing\TestResponse;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use PHPUnit\Framework\Assert;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        TestResponse::macro('data', function ($key) {
+            return $this->original->getData()[$key];
+        });
+
+        TestResponse::macro('assertSessionMissingErrors', function ($key, $bag = 'default') {
+            Assert::assertFalse(session()->has('errors') && session('errors')->getBag($bag)->has($key), "Failed asserting the session is missing the {$key} error.");
+        });
+
+        EloquentCollection::macro('assertContains', function ($value) {
+            Assert::assertTrue($this->contains($value), "Failed asserting that the collection contains the specified value.");
+        });
+
+        EloquentCollection::macro('assertNotContains', function ($value) {
+            Assert::assertFalse($this->contains($value), "Failed asserting that the collection does not contain the specified value.");
+        });
+
+        EloquentCollection::macro('assertEquals', function ($items) {
+            Assert::assertEquals(count($this), count($items));
+
+            $this->zip($items)->each(function ($pair) {
+                list($a, $b) = $pair;
+                Assert::assertTrue($a->is($b));
+            });
+        });
+    }
+
 }
