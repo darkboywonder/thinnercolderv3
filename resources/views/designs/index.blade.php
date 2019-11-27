3061f72f6cfc97e30205e8a3b982df45aec01e4a
Shawn Jones <shawn.greg.jones@gmail.com>
2019-11-27 00:18:28 -0600

    fix design link; add info drawer to designs

--- resources/views/designs/index.blade.php
+++ resources/views/designs/index.blade.php
@@ -8,7 +8,7 @@
     <div class="flex flex-wrap py-16 px-24">
         @foreach ($designs as $design)
             <div class="w-full md:w-1/2 lg:w-1/3 p-0 md:p-4">
-                <a class href="{{ route('artwork.show', $design) }}">
+                <a class href="{{ route('designs.show', $design) }}">
                     <img class="w-full shadow-md hover:shadow-xl" src="{{ Storage::url($design->image_thumb) }}" alt="{{ $design->name }} thumbnail image" />
 
                     <p class="font-poiret text-gray-900 sm:text-base md:text-lg text-2xl text-center my-2">
--- resources/views/designs/show.blade.php
+++ resources/views/designs/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('action')
-    <div><i class="fas fa-tags"></i></div>
+    <mobile-info-action-button></mobile-info-action-button>
 @endsection
 
 @section('content')
@@ -11,9 +11,9 @@
         <p class="text-center text-3xl text-black font-poiret mt-8 mb-4">{{ $design->name }}</p>
 
         <div class="shadow-lg mb-4
-            @if ($artwork->template == 'Square')
+            @if ($design->template == 'Square')
                 w-4/6
-            @elseif ($artwork->template == 'Landscape')
+            @elseif ($design->template == 'Landscape')
                 w-10/12
             @else
                 w-3/5
@@ -22,4 +22,25 @@
             <img class="w-full" src="{{ Storage::url($design->image) }}" alt="{{$design->name}} design" />
         </div>
     </div>
+
+    <info-drawer class="w-5/6 md:w-1/2">
+        <div class="flex h-24 md:h-32 items-center justify-center" style="background-color: {{ $design->color }}">
+            <h2 class=" font-poiret text-white text-3xl md:text-5xl text-center">{{ $design->name }}</h2>
+        </div>
+
+        <div class="flex flex-col items-center">
+            <h4 class="my-2 font-poiret text-2xl text-center text-slate">Client</h4>
+
+            <p class="text-slate text-center mb-16">{{ $design->client }}</p>
+
+            <h4 class="my-2 font-poiret text-2xl text-center text-slate">Design Category</h4>
+
+            <p class="text-slate text-center mb-16">{{ $design->category }}</p>
+
+            <h4 class="my-2 font-poiret text-2xl text-center text-slate">Type of Industry</h4>
+
+            <p class="text-slate text-center mb-16">{{ $design->type }}</p>
+
+        </div>
+    </info-drawer>
 @endsection
